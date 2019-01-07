02cb16201fa67c7d429279235a63019f35e518a7
rendnil <andrew.lindner.15@gmail.com>
2019-01-07 13:04:03 -0500

    updated trade validation

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -3,6 +3,6 @@ class Trade < ApplicationRecord
   belongs_to :asset
 
   ##need validations
-  validates :quantity, :numericality => {greater_than: 0}
+  validates :quantity, :numericality => {greater_than: 0, less_than_or_equal_to: 100}
 
 end
